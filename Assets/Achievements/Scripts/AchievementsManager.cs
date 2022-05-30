9ea4ce38f9c55e609e214f7aade46421ae1cc4ed
Oleh Halytskyi <olegexo@gmail.com>
2022-05-30 15:57:24 +0300

    Fixed inventory

--- Assets/Achievements/Scripts/AchievementsManager.cs
+++ Assets/Achievements/Scripts/AchievementsManager.cs
@@ -9,27 +9,35 @@ public class AchievementsManager : MonoBehaviour
     public float floating;
     private void Start()
     {
+        DontDestroyOnLoad(this);
         InitAchievements();
 
     }
     private void Update()
     {
         CheckCompltetion();
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            AchievementUnlocked("First steps1");
+        }
     }
-    public bool AchievementUnlocked(string achievementName)
+    public void AchievementUnlocked(string achievementName)
     {
-        bool results = false;
-        if(Achievements == null)
-            results = false;
-
         Achievement[] achievementsArray = Achievements.ToArray();
         Achievement a = Array.Find(achievementsArray, ach => achievementName == ach.Title);
-        if (a == null)
-            return false;
+        a.Achieved = true;
+        //bool results = false;
+        //if(Achievements == null)
+        //    results = false;
 
-        results = a.Achieved;
+        //Achievement[] achievementsArray = Achievements.ToArray();
+        //Achievement a = Array.Find(achievementsArray, ach => achievementName == ach.Title);
+        //if (a == null)
+        //    return false; 
 
-        return results;
+        //results = a.Achieved;
+
+        //return results;
     }
     private void CheckCompltetion()
     {
@@ -47,9 +55,25 @@ public class AchievementsManager : MonoBehaviour
         if(Achievements != null)
             return;
         Achievements = new List<Achievement>();
-        Achievements.Add(new Achievement("First steps", "Take your first step", (object o) => integer >= 100));
-        Achievements.Add(new Achievement("First steps", "Take your first step2", (object o) => integer >= 150));
-        Achievements.Add(new Achievement("First steps", "Take your first step3", (object o) => integer >= 200));
+        Achievements.Add(new Achievement("First steps1", "Take your first step", (object o) => integer >= 100));
+        Achievements.Add(new Achievement("First steps2", "Take your first step2", (object o) => integer >= 150));
+        Achievements.Add(new Achievement("First steps3", "Take your first step3", (object o) => integer >= 200));
+        Achievements.Add(new Achievement("First steps4", "Take your first step3", (object o) => integer >= 200));
+        //
+        Achievements.Add(new Achievement("First steps5", "Take your first step", (object o) => integer >= 100));
+        Achievements.Add(new Achievement("First steps6", "Take your first step2", (object o) => integer >= 150));
+        Achievements.Add(new Achievement("First steps7", "Take your first step3", (object o) => integer >= 200));
+        Achievements.Add(new Achievement("First steps8", "Take your first step3", (object o) => integer >= 200));
+        //
+        Achievements.Add(new Achievement("First steps9", "Take your first step", (object o) => integer >= 100));
+        Achievements.Add(new Achievement("First steps10", "Take your first step2", (object o) => integer >= 150));
+        Achievements.Add(new Achievement("First steps11", "Take your first step3", (object o) => integer >= 200));
+        Achievements.Add(new Achievement("First steps12", "Take your first step3", (object o) => integer >= 200));
+        //
+        Achievements.Add(new Achievement("First steps13", "Take your first step", (object o) => integer >= 100));
+        Achievements.Add(new Achievement("First steps14", "Take your first step2", (object o) => integer >= 150));
+        Achievements.Add(new Achievement("First steps15", "Take your first step3", (object o) => integer >= 200));
+        Achievements.Add(new Achievement("First steps16", "Take your first step3", (object o) => integer >= 200));
     }
 }
 public class Achievement
@@ -63,6 +87,7 @@ public class Achievement
         Title = title;
         Description = description;
         Requirement = requirement;
+        PlayerPrefs.SetString(title, "FALSE");
     }
     public void UpdateCompletion()
     {
